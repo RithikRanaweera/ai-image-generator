3a67ebdcc21f16f385e99b7fc6f42259ee6925a3
Rithik Ranaweera <rithikranaweera004@gmail.com>
2025-02-24 22:53:13 +0530

    create Steps, Testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Textimonials from '../components/Textimonials'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
+      <Description/>
+      <Textimonials/>
     </div>
   )
 }
