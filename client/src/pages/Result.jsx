0ac7b2a970c1e94f3f3392354eed682525174b8b
Rithik Ranaweera <rithikranaweera004@gmail.com>
2025-02-25 14:29:30 +0530

    Edit Result Page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -6,9 +6,14 @@ const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('')
+
+  const onSubmitHandler = async (e) => {
+    
+  }
 
   return (
-    <form className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <form className='flex flex-col min-h-[90vh] justify-center items-center' onSubmit={onSubmitHandler}>
       <div>
         <div className='relative'>
           <img src={image} alt="" className='max-w-sm rounded' />
@@ -21,7 +26,9 @@ const Result = () => {
       {!isImageLoaded &&
         <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
           <input type="text" placeholder='Describe what you want to generate'
-            className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
+            className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'
+            onChange={e => setInput(e.target.value)} 
+            value={input}/>
           <button type='submit'
             className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>
             Generate
