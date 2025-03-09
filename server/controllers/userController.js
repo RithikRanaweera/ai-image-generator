daa544d3b7d74c0891c6aa3a7bf4c5e4beb4c92a
Rithik Ranaweera <rithikranaweera004@gmail.com>
2025-03-09 22:07:11 +0530

    Create Middlewares

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -65,4 +65,22 @@ const loginUser = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser };
+const userCredits = async (req, res) => {
+    try{
+        const { userId } = req.body
+
+        const user = await userModel.findById(userId)
+        res.json({
+            sucess: true,
+            credits: user.creditBalance,
+            user: { name: user.name }
+        })
+    } catch(error){
+        console.log(error.message)
+        res.json({ sucess: false, message: error.message })
+    }
+}
+
+
+
+export { registerUser, loginUser, userCredits };
