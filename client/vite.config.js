f50428f79cfe18e4d0d82a29dd175a37ec970b1b
Nimesh Akarshana <masteraka524@gmail.com>
2025-04-03 23:55:40 +0530

    completed dockerize

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,4 +6,8 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',  // Ensures the server listens on all interfaces
+    port: 5173         // Bind the server to port 5173
+  }
 })
--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,12 +1,18 @@
 import exp from "constants";
+import express from "express";
 import mongoose from "mongoose";
 
-const connectDB = async () => {
 
-    mongoose.connection.on('connected', ()=>{
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => {
         console.log("Database Connected");
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
-}
+    });
+
+    // Connect using the MONGODB_URI from environment variables
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
 
 export default connectDB;
